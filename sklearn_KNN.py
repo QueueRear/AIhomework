ebdf2dd00be0b02e9147ce06d9308418720746f0
QueueRear <shenjdl@outlook.com>
2021-12-17 09:51:36 +0800

    修改数据处理部分，调用DataProcess类对输入进行处理

--- sklearn_KNN.py
+++ sklearn_KNN.py
@@ -1,24 +1,28 @@
 import numpy as np
 from os import error, listdir
-from loader import img2vector
+from DataProcess import DataProcess
 from sklearn import neighbors
 
+class NewDataProcess(DataProcess): # 继承数据处理类
+    def __init__(self, path):
+        super().__init__(path)
 
-def readDataset(path):
-    fileList = listdir(path)  # 获取当前文件夹下所有文件
-    n = len(fileList)
-    dataset = np.zeros([n, 1024], int)  # 存放数字文件
-    labels = np.zeros([n])  # 存放标签
-    for i in range(n):  # 遍历文件
-        filename = fileList[i]
-        digit = int(filename.split('_')[0])  # 文件名中包含了标签
-        labels[i] = digit  # 将文件对应的标签中存放数字
-        dataset[i] = img2vector(path + '/' + filename)  # 文件对应的向量保存向量化的图像
-    return dataset, labels
+    def readDataset(self):
+        fileList = listdir(self.path)  # 获取当前文件夹下所有文件
+        n = len(fileList)
+        dataset = np.zeros([n, 1024], int)  # 存放数字文件
+        labels = np.zeros([n])  # 存放标签
+        for i in range(n):  # 遍历文件
+            filename = fileList[i]
+            digit = int(filename.split('_')[0])  # 文件名中包含了标签
+            labels[i] = digit  # 将文件对应的标签中存放数字
+            dataset[i] = self.img2vector(self.path + '/' + filename)  # 文件对应的向量保存向量化的图像
+        return dataset, labels
 
 
 if __name__ == "__main__":
-    trainingDataset, trainingLabels = readDataset('dataset/trainingDigits')  # 加载训练数据
+    trainingDataset, trainingLabels = NewDataProcess(
+        'dataset/trainingDigits').readDataset()  # 加载训练数据
 
     # 构建 KNN 分类器
     # 设置查找算法及邻居点数量（k值）
@@ -27,9 +31,9 @@ if __name__ == "__main__":
     knn.fit(trainingDataset, trainingLabels)
 
     # 加载测试集
-    testDataset, testLabels = readDataset('dataset/testDigits')
+    testDataset, testLabels = NewDataProcess('dataset/testDigits').readDataset()
 
     ret = knn.predict(testDataset)
     error = np.sum(ret != testLabels)
     n = len(testDataset)
-    print("Total:", n, "Error:", error, "ErrorRate:", float(error) / n)
+    print("Total:", n, "Error:", error, "Accuracy:", float(n - error) / n)
--- sklearn_clf.py
+++ sklearn_clf.py
@@ -1,24 +1,25 @@
 import numpy as np
 from os import listdir
-from loader import img2vector
+from DataProcess import DataProcess
 from sklearn.neural_network import MLPClassifier
 from plot import plotLosslist
 
 
-def readDataset(path):  # 将样本标签转化为 one-hot 向量
-    fileList = listdir(path)  # 获取当前文件夹下所有文件
-    n = len(fileList)
-    dataset = np.zeros([n, 1024], int)  # 存放数字文件
-    labels = np.zeros([n, 10])  # 存放 one-hot 标签，将各维置零
-    for i in range(n):  # 遍历文件
-        filename = fileList[i]
-        digit = int(filename.split('_')[0])  # 文件名中包含了标签
-        labels[i][digit] = 1  # 将文件对应的向量对应的维度置一
-        dataset[i] = img2vector(path + '/' + filename)  # 文件对应的向量保存向量化的图像
-    return dataset, labels
+# def readDataset(path):  # 将样本标签转化为 one-hot 向量
+#     fileList = listdir(path)  # 获取当前文件夹下所有文件
+#     n = len(fileList)
+#     dataset = np.zeros([n, 1024], int)  # 存放数字文件
+#     labels = np.zeros([n, 10])  # 存放 one-hot 标签，将各维置零
+#     for i in range(n):  # 遍历文件
+#         filename = fileList[i]
+#         digit = int(filename.split('_')[0])  # 文件名中包含了标签
+#         labels[i][digit] = 1  # 将文件对应的向量对应的维度置一
+#         dataset[i] = img2vector(path + '/' + filename)  # 文件对应的向量保存向量化的图像
+#     return dataset, labels
 
 if __name__ == "__main__":
-    trainingDataset, trainingLabels = readDataset('dataset/trainingDigits')  # 加载训练数据
+    trainingDataset, trainingLabels = DataProcess(
+        'dataset/trainingDigits').readDataset()  # 加载训练数据
 
     # 设置超参数
     # 设置一个有100个神经元的隐藏层
@@ -31,13 +32,14 @@ if __name__ == "__main__":
     plotLosslist(lossList, "Loss of sklearn_clf : alpha=" + str(alpha))
 
     # 加载测试集
-    testDataset, testLabels = readDataset('dataset/testDigits')
+    testDataset, testLabels = DataProcess('dataset/testDigits').readDataset()
 
     ret = clf.predict(testDataset) # 让训练出来的模型在测试集上跑
-    error = 0 # 计算判断错误的数量
+    accurate = 0  # 计算判断正确的数量
     n = len(testDataset)
     for i in range(n):
         # ret[i] == testLabels[i] 会返回一个长度为10的数组，1 表示对应元素相同
-        if np.sum(ret[i] == testLabels[i]) < 10:
-            error += 1
-    print("Total:", n, "Error:", error, "Accuracy:", float(n - error) / n)
+        # if np.sum(ret[i] == testLabels[i]) < 10:
+        #     error += 1
+        accurate += int(np.argmax(ret[i]) == np.argmax(testLabels[i]))
+    print("Total:", n, "Error:", n - accurate, "Accuracy:", float(accurate * 100) / n, "%")
